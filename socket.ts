7f8b8a838ae8920e287d604d021faec16f94e255
deadeye03 <yakshitkr@gmail.com>
2025-01-28 23:52:09 +0530

    update socket

--- socket.ts
+++ socket.ts
@@ -2,7 +2,9 @@
 
 import { io } from "socket.io-client"
 
-export const socket = io("http://localhost:3001", {
+if(!process.env.NEXT_PUBLIC_SOCKET_URL) throw new Error("NEXT_PUBLIC_SOCKET_URL is not defined")
+  
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
   transports: ["websocket", "polling"],
 })
 
